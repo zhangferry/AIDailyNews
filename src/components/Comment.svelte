7af5c2877f31b96e6271e8e7067a9cecc99ff199
zhangferry <457030189@qq.com>
2024-06-01 16:15:03 +0800

    fix: add PUBLIC prefix for env

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -5,11 +5,11 @@
 
   onMount(() => {
     const gitalk = new Gitalk({
-      clientID: import.meta.env.GITTALK_CLIENT_ID,
-      clientSecret: import.meta.env.GITTALK_CLIENT_SECRET,
-      repo: import.meta.env.GITTALK_REPO, // The repository of store comments,
-      owner: import.meta.env.GITTALK_OWNER,
-      admin: [import.meta.env.GITTALK_OWNER],
+      clientID: import.meta.env.PUBLIC_GITTALK_CLIENT_ID,
+      clientSecret: import.meta.env.PUBLIC_GITTALK_CLIENT_SECRET,
+      repo: import.meta.env.PUBLIC_GITTALK_REPO, // The repository of store comments,
+      owner: import.meta.env.PUBLIC_GITTALK_OWNER,
+      admin: [import.meta.env.PUBLIC_GITTALK_OWNER],
       id: location.pathname, // Ensure uniqueness and length less than 50
       distractionFreeMode: false, // Facebook-like distraction free mode
     });
