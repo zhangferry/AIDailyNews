9be71fdae4b166abbac41e521b7b96a9046a8a5e
zhangferry <457030189@qq.com>
2024-06-01 12:13:25 +0800

    feat: config gittalk with env

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -3,19 +3,16 @@
   import "gitalk/dist/gitalk.css";
   import Gitalk from "gitalk";
 
-  console.error("on gitalk");
   onMount(() => {
     const gitalk = new Gitalk({
-      clientID: "012999c912503aff4cac",
-      clientSecret: "fcc9ff4a4ca14d80b38de2548cd1102bf7147c57",
-      repo: "AIDailyNews", // The repository of store comments,
-      owner: "zhangferry",
-      admin: ["zhangferry"],
+      clientID: import.meta.env.GITTALK_CLIENT_ID,
+      clientSecret: import.meta.env.GITTALK_CLIENT_SECRET,
+      repo: import.meta.env.GITTALK_REPO, // The repository of store comments,
+      owner: import.meta.env.GITTALK_OWNER,
+      admin: [import.meta.env.GITTALK_OWNER],
       id: location.pathname, // Ensure uniqueness and length less than 50
       distractionFreeMode: false, // Facebook-like distraction free mode
     });
-
-    console.error("on mount gitalk");
     gitalk.render("gitalk-container");
   });
 </script>
