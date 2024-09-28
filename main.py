58756b5f2697d721f4004902c644c43e788caeff
zhangferry <zhangferry11@gmail.com>
2024-09-28 18:24:47 +0800

    chore: add envs log

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import workflow.mainflow as mainflow
 from dotenv import load_dotenv
+from loguru import logger
 import os
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     load_dotenv()
+    logger.info(f"envs: {os.environ}")
 
     # os.environ["https_proxy"] = "http://127.0.0.1:465"
     # os.environ["http_proxy"] = "http://127.0.0.1:465"
